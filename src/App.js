ca43ff4e49b99e08b502afb749d78fae1f74aee3
Abhijeet <94052679+iamTheAvyzit@users.noreply.github.com>
2022-01-14 09:35:01 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Holdings from './components/Holdings';
+import Transactions from './components/Transactions';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Holdings/>
+      <Transactions/>
+    </div>
+  );
+}
+
+export default App;
