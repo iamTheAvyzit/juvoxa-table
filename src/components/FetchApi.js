af600fd4d2227c618902e753479435fce4efa719
iamTheAvyzit <iamTheAvyzit>
2022-01-15 00:41:38 -0500

    second commit for juvoxa

--- src/components/FetchApi.js
+++ src/components/FetchApi.js
@@ -1,9 +1,11 @@
 export const getHoldingsPayload = async () => {
     return await fetch('https://canopy-frontend-task.vercel.app/api/holdings')
     .then((res) => res.json())
+    .catch(err => alert(err))
 }
 
 export const getTransactions = async () => {
     return await fetch('https://canopy-frontend-task.vercel.app/api/transactions')
     .then((res) => res.json())
+    .catch(err => alert(err))
 }
